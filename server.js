02b84d67fddde333bde3ae60ff2b3ca455a2dcf1
gstout13 <gstout13@uw.edu>
2025-03-10 12:44:04 -0700

    first push attempt

---
+++ server.js
@@ -0,0 +1,15 @@
+// Import the 'app' module from the './src/app' file
+
+let corsOptions = {
+  origin: ['https://127.0.0.1', 'https://www.github.io', 'https://jakobzhao.github.io'],
+  optionsSuccessStatus: 200
+  }
+const app = require('./src/app');
+
+// Set the port for the server to listen on, defaulting to 3000 if not provided
+const port = process.env.PORT || 3000;
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log('application running on', port);
+});
